7c08595967e53acbc602b6be8ef56a32e43be03d
thedanielvogelsang <dr.vogelsang26@gmail.com>
2020-08-05 06:32:24 -0500

    basic user tests passing, ready for TinyUrl model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  before_validation :create_tiny_url, if: -> { !tiny_url }
+
+  validates :name, presence: true
+  validates :url, presence: true
+  validates :tiny_url, presence: true
+
+  def create_tiny_url
+    self.tiny_url = url
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,6 +7,15 @@ RSpec.describe User, type: :model do
   describe 'validations' do
     it { expect(subject).to validate_presence_of(:name) }
     it { expect(subject).to validate_presence_of(:url) }
-    it { expect(subject).to validate_presence_of(:shortened_url) }
+    it { expect(subject).to validate_presence_of(:tiny_url) }
+  end
+
+  describe 'creation' do
+    let(:user) { User.new(name: Faker::Name.name, url: Faker::Internet.domain_name(subdomain: "example")) }
+
+    it 'can create a user with name and url' do
+      user.save
+      expect(user.errors.empty?).to eq(true)
+    end
   end
 end
