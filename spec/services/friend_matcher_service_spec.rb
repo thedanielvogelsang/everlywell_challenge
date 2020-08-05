18d6aa054a7bf8b9aeb513da0e6ce85f368a3fd9
thedanielvogelsang <dr.vogelsang26@gmail.com>
2020-08-05 08:51:40 -0500

    adds tests for new matcher service init

---
+++ spec/services/friend_matcher_service_spec.rb
@@ -0,0 +1,51 @@
+require 'rails_helper'
+
+RSpec.describe FriendMatcherService, type: :service do
+  before(:all) do
+    website_options = %w(
+      https://www.theguardian.com/us
+      www.coloradosolidarity.com
+      https://www.thorn.org/about-our-fight-against-sexual-exploitation-of-children/
+      www.practicalbioethics.org/
+    )
+
+    User.destroy_all
+
+    4.times do |i|
+      user = User.create(name: Faker::Name.name, url: website_options[i])
+    end
+
+    User.first.add_friend(User.second)
+    User.second.add_friend(User.last)
+  end
+
+  let(:subject) { FriendMatcherService.new(search_user: user1, new_friend: user2) }
+  let!(:user) { User.first }
+  let(:friend_of_user) { user.friends.first }
+  let(:medical_expert_non_friend) { User.last }
+
+  describe 'initialization' do
+    let(:error) { FriendMatcherService::FriendError.new(user1, user2) }
+    let(:call) { subject }
+
+    before :each do
+      allow(FriendMatcherService::FriendError).to receive(:new).and_raise(error)
+    end
+
+    context 'with two users who ARE friends' do
+      let(:user1) { user }
+      let(:user2) { friend_of_user }
+      it 'cant initialize with two users who are friends' do
+        expect{ call }.to raise_error(error)
+      end
+    end
+
+    context 'with two users who ARE friends' do
+      let(:user1) { user }
+      let(:user2) { medical_expert_non_friend }
+      it 'can initialize with two users who are not friends' do
+        expect{ call }.to_not raise_error
+      end
+    end
+  end
+end
