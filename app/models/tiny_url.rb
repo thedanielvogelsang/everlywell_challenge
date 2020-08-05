59d6ab742bdfdae306367e45558a7b912b557458
thedanielvogelsang <dr.vogelsang26@gmail.com>
2020-08-05 06:51:43 -0500

    adds tests for shortened_url and sanitized_url formatting/validations

--- app/models/tiny_url.rb
+++ app/models/tiny_url.rb
@@ -1,5 +1,5 @@
 class TinyUrl < ApplicationRecord
-  TINY_URL_LENGTH = 8
+  TINY_URL_LENGTH = 7
 
   validates :original_url, presence: true, format: {
     with: /\A(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?\z/i,
--- spec/models/tiny_url_spec.rb
+++ spec/models/tiny_url_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe TinyUrl, type: :model do
   describe 'constants and validations' do
     it { expect(subject).to validate_presence_of(:original_url) }
     it { expect(subject).to_not validate_presence_of(:shortened_url) }
-    it { expect(TinyUrl::TINY_URL_LENGTH).to eq(8) }
+    it { expect(TinyUrl::TINY_URL_LENGTH).to eq(7) }
   end
 
   describe 'creation' do
@@ -63,6 +63,35 @@ RSpec.describe TinyUrl, type: :model do
 
         it 'creates a sanitized_url from the original_url' do
           expect(new_url_record.sanitized_url.blank?).to eq(false)
+        end
+      end
+    end
+
+    describe 'shortened_url' do
+      let!(:new_url_record) { TinyUrl.create(original_url: not_my_url) }
+      let!(:other_shortened_url) { TinyUrl.new(original_url: prince_gently_weeps, shortened_url: new_url_record.shortened_url)}
+
+      let(:my_url) { prince_gently_weeps }
+      let(:not_my_url) { prince_gently_weeps + 'ABCDEF'}
+      let(:prince_gently_weeps) { 'https://www.youtube.com/watch?v=6SFNW5F8K9Y' }
+
+      context 'upon creation' do
+        let(:shortened_url_length) { 'http://'.length + TinyUrl::TINY_URL_LENGTH }
+
+        it 'creates a shortened_url to the given constant length' do
+          expect(new_url_record.shortened_url.length).to eq(shortened_url_length)
+        end
+      end
+    end
+
+    describe 'sanitized_url' do
+      let(:my_url) { prince_gently_weeps }
+      context 'upon creation' do
+        let(:prince_gently_weeps) { 'https://www.youtube.com/watch?v=6SFNW5F8K9Y' }
+        let(:sanitized_url_format) { Regexp.new(/^(http\:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?\z/) }
+
+        it 'creates a sanitized_url that matches desired format' do
+          expect(sanitized_url_format.match(new_url_record.sanitized_url)).to be_truthy
         end
       end
     end
