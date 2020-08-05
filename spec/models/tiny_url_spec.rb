730078d818f37931e5fc2a9403f49ff4f9c1c470
thedanielvogelsang <dr.vogelsang26@gmail.com>
2020-08-05 06:44:06 -0500

    tests callback

--- spec/models/tiny_url_spec.rb
+++ spec/models/tiny_url_spec.rb
@@ -9,42 +9,57 @@ RSpec.describe TinyUrl, type: :model do
 
   describe 'creation' do
     let(:new_shortened_url) { TinyUrl.new(original_url: my_url) }
+    let(:new_url_record) { TinyUrl.create(original_url: my_url) }
 
-    context 'invalid url' do
-      describe 'random string' do
-        let(:my_url) { 'adsfasdf;ads' }
+    describe 'url validations' do
+      context 'invalid url' do
+        describe 'random string' do
+          let(:my_url) { 'adsfasdf;ads' }
 
-        it 'should return an error' do
-          new_shortened_url.save
-          expect(new_shortened_url.errors.messages[:original_url]).to eq(["Invalid url"])
+          it 'should return an error' do
+            new_shortened_url.save
+            expect(new_shortened_url.errors.messages[:original_url]).to eq(["Invalid url"])
+          end
+        end
+
+        describe 'email' do
+          let(:my_url) { 'www.myexamplesite@mydomain.com' }
+
+          it 'should return an error' do
+            new_shortened_url.save
+            expect(new_shortened_url.errors.messages[:original_url]).to eq(["Invalid url"])
+          end
+        end
+
+        describe 'capital letters' do
+          let(:my_url) { 'www.myexamplesite@mydomain.com'.upcase }
+
+          it 'should return an error' do
+            new_shortened_url.save
+            expect(new_shortened_url.errors.messages[:original_url]).to eq(["Invalid url"])
+          end
         end
       end
 
-      describe 'email' do
-        let(:my_url) { 'www.myexamplesite@mydomain.com' }
+      context 'valid url' do
+        let(:my_url) { 'www.myexamplesite.mydomain.com' }
 
         it 'should return an error' do
           new_shortened_url.save
-          expect(new_shortened_url.errors.messages[:original_url]).to eq(["Invalid url"])
-        end
-      end
-
-      describe 'capital letters' do
-        let(:my_url) { 'www.myexamplesite@mydomain.com'.upcase }
-
-        it 'should return an error' do
-          new_shortened_url.save
-          expect(new_shortened_url.errors.messages[:original_url]).to eq(["Invalid url"])
+          expect(new_shortened_url.errors.messages.empty?).to eq(true)
         end
       end
     end
 
-    context 'valid url' do
-      let(:my_url) { 'www.myexamplesite.mydomain.com' }
+    describe 'callbacks' do
+      let(:my_url) { supa_hot_fire }
 
-      it 'should return an error' do
-        new_shortened_url.save
-        expect(new_shortened_url.errors.messages.empty?).to eq(true)
+      context 'before create callbacks' do
+        let(:supa_hot_fire) { 'https://www.youtube.com/watch?v=Iu90z9Akxgk&t=201s'}
+
+        it 'creates a shortened_url from the original_url' do
+          expect(new_url_record.shortened_url.blank?).to eq(false)
+        end
       end
     end
   end
