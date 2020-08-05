1f16f23ccb8df2137ad3d23db3b8a6cf411858c3
thedanielvogelsang <dr.vogelsang26@gmail.com>
2020-08-05 11:48:59 -0500

    fixes routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resource :searches, only: [:create]
+      resources :searches, only: [:create]
       resources :users, only: [:index, :show, :create]
     end
   end
-  resource :users, only: [:index, :show]
+  resources :users, only: [:index, :show]
   get 'tiny_urls/show'
   post 'find_expert', to: 'api/v1/searches#find_expert' # find_expert_path
 end
